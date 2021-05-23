f1d44224b2308a4fe13680ababd7daa7e73e8cf4
woooyoung <axdsw121@gmail.com>
2021-05-23 09:40:03 +0900

    가로메뉴 03(가상)

---
+++ 07 - 가로메뉴_3_1/script/common01.js
@@ -0,0 +1,11 @@
+  $(document).ready(function () {
+
+      $(".menu > ul > li").mouseover(function () {
+          $(".menu > ul > li > ul").show();
+
+      });
+      $(".menu > ul > li").mouseout(function () {
+          $(".menu > ul > li > ul").hide();
+
+      });
+  });
